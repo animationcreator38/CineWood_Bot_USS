c6ce214642649061eb24fa6c71b0e6bb7f8b5d22
animationcreator38 <147336591+animationcreator38@users.noreply.github.com>
2023-11-01 11:16:48 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -128,7 +128,7 @@ async def disable_chat(bot, message):
     await message.reply('Chat Successfully Disabled')
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url=f'https://t.me/{SUPPORT_CHAT}')
+            InlineKeyboardButton('CONTACT ME', url='https://t.me/cinewood_1')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
